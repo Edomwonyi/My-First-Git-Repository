1c4b0d5ec2ddd1f0b8030184f2366765fbc13d28
ScottMan <scottedomwonyi@Gmail.com>
2018-03-08 14:53:15 +0200

    Just Added A New VStudio Program.cs.

---
+++ LearningToUseGitHub/NowTimeForHW/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NowTimeForHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("yes we are good");
+        }
+    }
+}
